cd91002712dc859f1308223b398991596201e2ae
jonatan <jonatan.haaralaorosco@gmail.com>
2022-08-20 13:05:43 -0300

    sprint1 controllers

---
+++ InmoAPI/Controllers/TesoreriaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InmoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TesoreriaController : ControllerBase
+    {
+    }
+}
